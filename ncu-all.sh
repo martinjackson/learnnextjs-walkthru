d6244c9e16d17fa98857128b5dca1c1c208419fc
Martin A. Jackson <martin.a.jackson@gmail.com>
2020-03-31 10:02:17 -0500

    upgraded all packages to latest

---
+++ ncu-all.sh
@@ -0,0 +1,19 @@
+
+BASE=/home/mjackson/training/learnnextjs-walkthru
+
+cd $BASE
+
+# $ find . -name package.json
+
+cd basics-lesson-1-Getting-Started/hello-next                          ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-2-Navigate-Between-Pages/hello-next                   ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-2-Navigate-Between-Pages/learnnextjs-demo             ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-3-Using-Shared-Components/learnnextjs-demo            ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-4-Create-Dynamic-Pages/learnnextjs-demo               ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-5-Clean-URLs-with-Route-Masking/learnnextjs-demo      ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-6-Server-Side-Support-for-Clean-URLs/learnnextjs-demo ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-7-Fetching-Data-for-Pages/learnnextjs-demo            ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-8-Styling-Components/learnnextjs-demo                 ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-9B-Deploying-a-NextJS-App/learnnextjs-demo            ;  ncu -u ; npm install ; cd $BASE
+cd basics-lesson-9-Deploying-a-NextJS-App/learnnextjs-demo             ;  ncu -u ; npm install ; cd $BASE
+cd excel-1-Export-into-a-Static-HTML-App/learnnextjs-demo              ;  ncu -u ; npm install ; cd $BASE
